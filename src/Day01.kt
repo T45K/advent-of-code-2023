cfbd35fb3c1674f5ecbcd68b958c6979ed59b9d9
T45K <tasktas9@gmail.com>
2023-12-03 20:56:59 +0900

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,10 +4,10 @@ import readInput
 
 fun main() {
     val input = readInput("input/Day01.txt")
-    val answer = input.map {
+    val answer = input.sumOf {
         val firstDigit = it.find(Char::isDigit).toString()
         val lastDigit = it.findLast(Char::isDigit).toString()
         (firstDigit + lastDigit).toInt()
-    }.sum()
+    }
     println(answer)
 }
