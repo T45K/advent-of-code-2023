bd8e6398c602badb345f72b67a55d3847a132129
T45K <tasktas9@gmail.com>
2023-12-03 22:39:42 +0900

    day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,10 +4,28 @@ import readInput
 
 fun main() {
     val input = readInput("input/Day01.txt")
-    val answer = input.sumOf {
-        val firstDigit = it.find(Char::isDigit).toString()
-        val lastDigit = it.findLast(Char::isDigit).toString()
-        (firstDigit + lastDigit).toInt()
+    val searchTargets = (1..9).map { it.toString() } + digitMap.keys
+    val answer = input.sumOf { line ->
+        val firstDigit = line.findAnyOf(searchTargets)!!.second.toDigit()
+        val lastDigit = line.findLastAnyOf(searchTargets)!!.second.toDigit()
+        firstDigit * 10 + lastDigit
     }
     println(answer)
 }
+
+private fun String.toDigit(): Int = when {
+    this in digitMap -> digitMap[this]!!
+    else -> this.toInt()
+}
+
+private val digitMap = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
