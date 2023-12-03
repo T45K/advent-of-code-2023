e81bfa780b0545e91a31fa263bb33bd75f1f9acf
T45K <tasktas9@gmail.com>
2023-12-03 23:08:35 +0900

    day03 part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -6,23 +6,39 @@ import readInput
 
 fun main() {
     val input = readInput("input/Day03.txt")
+    val answer = part2(input)
+    println(answer)
+}
+
+private fun part1(input: List<String>): Long {
     val schematic = Schematic(input)
-    val answer = (0..<schematic.rowSize).map { rowIndex ->
+    return (0..<schematic.rowSize).sumOf { rowIndex ->
         schematic.enumerateNumberRanges(rowIndex)
             .filter { schematic.isAdjacentToSymbols(rowIndex, it) }
             .mapNotNull { schematic.extractNumber(rowIndex, it) }
             .sum()
-    }.sum()
-    println(answer)
+    }
+}
+
+private fun part2(input: List<String>): Long {
+    val schematic = Schematic(input)
+    return (0..<schematic.rowSize).flatMap { rowIndex ->
+        schematic.enumerateNumberRanges(rowIndex)
+            .mapNotNull { range -> schematic.findAdjacentGear(rowIndex, range)?.let { Triple(it, rowIndex, range) } }
+    }.groupBy({ (point, _, _) -> point }) { (_, rowIndex, range) -> rowIndex to range }
+        .values
+        .filter { it.size == 2 }
+        .sumOf { (range1, range2) ->
+            schematic.extractNumber(range1.first, range1.second)!! *
+                schematic.extractNumber(range2.first, range2.second)!!
+        }
 }
 
 private class Schematic(private val rows: List<String>) {
     val rowSize = rows.size
     private val colSize = rows[0].length
 
-    private val symbolMap: List<List<Boolean>> = rows.map { str ->
-        str.toCharArray().map { !(it.isDigit() || it == '.') }
-    }
+    private fun Char.isSymbol() = !(this.isDigit() || this == '.')
 
     fun extractNumber(rowIndex: Int, range: IntRange): Long? {
         require(
@@ -53,11 +69,28 @@ private class Schematic(private val rows: List<String>) {
         )
         return (max(rowIndex - 1, 0)..min(rowIndex + 1, rowSize - 1)).any { x ->
             (max(range.first - 1, 0)..min(range.last + 1, colSize - 1)).any { y ->
-                symbolMap[x][y]
+                rows[x][y].isSymbol()
             }
         }
     }
+
+    fun findAdjacentGear(rowIndex: Int, range: IntRange): Point? {
+        require(
+            rowIndex in 0..<rowSize &&
+                0 <= range.first && range.last < colSize
+        )
+        for (x in max(rowIndex - 1, 0)..min(rowIndex + 1, rowSize - 1)) {
+            for (y in max(range.first - 1, 0)..min(range.last + 1, colSize - 1)) {
+                if (rows[x][y] == '*') {
+                    return Point(x, y)
+                }
+            }
+        }
+        return null
+    }
 }
+
+private data class Point(val x: Int, val y: Int)
 
 /**
  * To avoid using Iterator<T>.plus(v: Iterator<T>) instead of Iterator<T>.plus(v: T)
