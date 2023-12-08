bcf9c1cab4381e6817ab6bb5e29fa6995d1ab68a
T45K <tasktas9@gmail.com>
2023-12-09 00:38:54 +0900

    day08 part2

--- src/Day08.kt
+++ src/Day08.kt
@@ -4,24 +4,76 @@ import readInput
 
 fun main() {
     val input = readInput("input/Day08.txt")
+    val answer = part2(input)
+    println(answer)
+}
+
+private fun part1(input: List<String>): Long {
     val route = input[0]
     val map = input.subList(2, input.size)
-        .associate { line ->
+        .map { Navigation.fromLine(it) }
+        .associateBy { it.current }
+    return findShortestStepCount(route, map, Node("AAA")) { it == Node("ZZZ") }
+}
+
+private fun part2(input: List<String>): Long {
+    val route = input[0]
+    val map = input.subList(2, input.size)
+        .map { Navigation.fromLine(it) }
+        .associateBy { it.current }
+    val shortestPaths = map.keys.filter { it.value.last() == 'A' }
+        .map { initialNode -> findShortestStepCount(route, map, initialNode) { it.value.last() == 'Z' } }
+    return shortestPaths.reduce(::calcLcm)
+}
+
+private fun calcLcm(a: Long, b: Long): Long {
+    val gcd = euclideanAlgorithm(a, b)
+    return a * b / gcd
+}
+
+private fun euclideanAlgorithm(a: Long, b: Long): Long =
+    when {
+        b > a -> euclideanAlgorithm(b, a)
+        b == 0L -> a
+        else -> euclideanAlgorithm(b, a % b)
+    }
+
+private fun findShortestStepCount(
+    route: String,
+    map: Map<Node, Navigation>,
+    initialNode: Node,
+    reachPredicate: (Node) -> Boolean
+): Long {
+    return generateSequence(0L to initialNode) { (count, current) ->
+        val next = if (route[(count % route.length).toInt()] == 'L') {
+            map[current]!!.left
+        } else {
+            map[current]!!.right
+        }
+        count + 1 to next
+    }.first { (_, reachedNode) -> reachPredicate(reachedNode) }
+        .let { (count, _) -> count }
+}
+
+private data class Navigation private constructor(
+    val current: Node,
+    val left: Node,
+    val right: Node,
+) {
+    companion object {
+        fun fromLine(line: String): Navigation =
             "([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)".toRegex()
                 .find(line)!!
                 .groupValues
                 .let {
-                    it[1] to (it[2] to it[3])
+                    Navigation(Node(it[1]), Node(it[2]), Node(it[3]))
                 }
-        }
-    val answer = generateSequence(0 to "AAA") { (count, current) ->
-        val next = if (route[count % route.length] == 'L') {
-            map[current]!!.first
-        } else {
-            map[current]!!.second
-        }
-        count + 1 to next
-    }.first { it.second == "ZZZ" }
-        .first
-    println(answer)
+    }
+}
+
+@JvmInline
+value class Node(val value: String) {
+    init {
+        require(value.length == 3)
+    }
 }
