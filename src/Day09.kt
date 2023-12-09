355db47bc28e4508c5a22ea01782ff933dfe28ab
T45K <tasktas9@gmail.com>
2023-12-09 16:51:29 +0900

    day09 part2

--- src/Day09.kt
+++ src/Day09.kt
@@ -4,20 +4,37 @@ import readInput
 
 fun main() {
     val input = readInput("input/Day09.txt")
-    val answer = input.map { it.split(" ").map { it.toLong() } }
-        .map { predicateNextNumber(it) }
-        .reduce(Long::plus)
+    val answer = part2(input)
     println(answer)
 }
 
-private fun predicateNextNumber(initialNumbers: List<Long>): Long {
-    val numbersList: List<List<Long>> = generateSequence(listOf(initialNumbers)) { acc: List<List<Long>> ->
-        val last = acc.last()
-        val next = (0..<last.size - 1).map { last[it + 1] - last[it] }
-        acc.plus<List<Long>>(next)
-    }.first { numbersList -> numbersList.last().all { it == 0L } }
+private fun part1(input: List<String>): Long =
+    input.map { it.split(" ").map(String::toLong) }
+        .map(::predicateNextNumber)
+        .reduce(Long::plus)
 
+private fun part2(input: List<String>): Long =
+    input.map { it.split(" ").map(String::toLong) }
+        .map(::predicatePreviousNumber)
+        .reduce(Long::plus)
+
+private fun predicateNextNumber(initialNumbers: List<Long>): Long {
+    val numbersList: List<List<Long>> = reproduceHistory(initialNumbers)
     return numbersList.reversed().fold(0L) { acc, numbers ->
         acc + numbers.last()
     }
 }
+
+private fun predicatePreviousNumber(initialNumbers: List<Long>): Long {
+    val numbersList: List<List<Long>> = reproduceHistory(initialNumbers)
+    return numbersList.reversed().fold(0L) { acc, numbers ->
+        numbers.first() - acc
+    }
+}
+
+private fun reproduceHistory(initialNumbers: List<Long>): List<List<Long>> =
+    generateSequence(listOf(initialNumbers)) { acc: List<List<Long>> ->
+        val last = acc.last()
+        val next = (0..<last.size - 1).map { last[it + 1] - last[it] }
+        acc.plus<List<Long>>(next)
+    }.first { numbersList -> numbersList.last().all { it == 0L } }
