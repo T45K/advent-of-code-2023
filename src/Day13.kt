99c780c8c55457309c103b3c09a7f28ba61c5a16
T45K <tasktas9@gmail.com>
2023-12-16 22:30:17 +0900

    day13 part2

--- src/Day13.kt
+++ src/Day13.kt
@@ -6,8 +6,13 @@ import transpose
 
 fun main() {
     val input = readInput("input/Day13.txt")
+    val answer = part2(input)
+    println(answer)
+}
+
+private fun part1(input: List<String>): Int {
     val maps = input.split { it.isBlank() }.map { line -> line.map { it.toCharArray().toList() } }
-    val answer = maps.sumOf { map ->
+    return maps.sumOf { map ->
         val horizontalNum = findMirrorLine(map)
 
         if (horizontalNum != null) {
@@ -17,7 +22,20 @@ fun main() {
         val transposedMap = map.transpose()
         findMirrorLine(transposedMap)!!
     }
-    println(answer)
+}
+
+private fun part2(input: List<String>): Int {
+    val maps = input.split { it.isBlank() }.map { line -> line.map { it.toCharArray().toList() } }
+    return maps.sumOf { map ->
+        val horizontalNum = findSmudgedMirrorLine(map)
+
+        if (horizontalNum != null) {
+            return@sumOf horizontalNum * 100
+        }
+
+        val transposedMap = map.transpose()
+        findSmudgedMirrorLine(transposedMap)!!
+    }
 }
 
 private fun findMirrorLine(map: List<List<Char>>): Int? = (1..<map.size).find { num: Int ->
@@ -25,4 +43,18 @@ private fun findMirrorLine(map: List<List<Char>>): Int? = (1..<map.size).find { 
     generateSequence(0) { it + 1 }
         .takeWhile { index - it >= 0 && index + it + 1 < map.size }
         .all { map[index - it] == map[index + 1 + it] }
+}
+
+private fun findSmudgedMirrorLine(map: List<List<Char>>): Int? = (1..<map.size).find { num: Int ->
+    val index = num - 1
+    val differentLineNumbers = generateSequence(0) { it + 1 }
+        .takeWhile { index - it >= 0 && index + it + 1 < map.size }
+        .filterNot { map[index - it] == map[index + 1 + it] }
+        .toList()
+    if (differentLineNumbers.size == 1) {
+        val differentLineNumber = differentLineNumbers[0]
+        map[index - differentLineNumber].zip(map[index + differentLineNumber + 1]).count { (a, b) -> a != b } == 1
+    } else {
+        false
+    }
 }
